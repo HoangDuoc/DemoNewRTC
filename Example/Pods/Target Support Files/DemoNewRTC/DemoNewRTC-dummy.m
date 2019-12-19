1c6b2de10e221dd47ae0cb28355eb8d568f57a29
stringeecom <info@stringee.com>
2019-12-19 16:04:45 +0700

    Initial commit

---
+++ Example/Pods/Target Support Files/DemoNewRTC/DemoNewRTC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DemoNewRTC : NSObject
+@end
+@implementation PodsDummy_DemoNewRTC
+@end
---
+++ Example/Pods/Target Support Files/Pods-DemoNewRTC_Tests/Pods-DemoNewRTC_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoNewRTC_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoNewRTC_Tests
+@end
